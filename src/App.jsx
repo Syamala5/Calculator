894ce9424543d128dc08dac7a66571215c0ae6ed
Syamala5 <syamalakirla2002@gmail.com>
2024-12-30 09:15:38 +0530

    fcommit calci

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Calci from './components/Calci'
+
+function App() {
+
+
+  return (
+    <div className='bg-[#141414] min-h-screen text-white flex items-center justify-center'>
+      <Calci />
+    </div>
+  )
+}
+
+export default App
